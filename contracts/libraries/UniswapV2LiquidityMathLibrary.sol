9d44d9582cde4200afe8b871967c8a329fc6d590
Тимерьян Насибуллин <-github-email@example.com>
2024-04-17 20:40:33 +0500

    upd

--- contracts/libraries/UniswapV2LiquidityMathLibrary.sol
+++ contracts/libraries/UniswapV2LiquidityMathLibrary.sol
@@ -1,9 +1,9 @@
 pragma solidity >=0.5.0;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-import '@uniswap/lib/contracts/libraries/Babylonian.sol';
-import '@uniswap/lib/contracts/libraries/FullMath.sol';
+import '@atleta-chain/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import '@atleta-chain/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
+import '@atleta-chain/lib/contracts/libraries/Babylonian.sol';
+import '@atleta-chain/lib/contracts/libraries/FullMath.sol';
 
 import './SafeMath.sol';
 import './UniswapV2Library.sol';
